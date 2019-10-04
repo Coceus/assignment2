42a4c7989b2a458dc758b66ed994917e2b7e08d4
Coceus <46695012+Coceus@users.noreply.github.com>
2019-10-04 20:13:03 -0100

    Skate Board h minor corection

--- Skateboard.h
+++ Skateboard.h
@@ -1,1 +1,23 @@
-#pragma once
+
+#ifndef DRIVINGSIMULATOR_Skateboard_H
+#define DRIVINGSIMULATOR_Skateboard_H
+#include <cstdlib>
+#include "Vehicle.h"
+
+class Skateboard : public Vehicle {
+
+private:
+	int myGearCount;
+
+public:
+	explicit Skateboard(string brand, string model);
+
+	virtual ~Skateboard();
+	
+	virtual double mileageEstimate(double time);
+
+	virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_Skateboard_H
